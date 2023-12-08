bbb59bf2e56dd28b50871bce257df8e7baf96271
Taiga <taiga507@gmail.com>
2023-12-08 21:07:45 +0400

    5-103
    ES6 Modules

---
+++ 5-103_ES6_Modules/src/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// import {one, two} from './main';
+// console.log(`${one} and ${two}`);
+
+// import {one as first} from './main';
+
+// import * as data from 'main.js';
+// console.log(`${data.one} and ${data.two}`);
+
+import sayHi from './main.js';
+sayHi();
