2b22a7771b2adcdb8872481932955f8c30a73e0c
Taiga <taiga507@gmail.com>
2023-12-12 23:24:54 +0400

    5-110
    Jquery_library

---
+++ taiga-d/Food/src/js/main.js
@@ -0,0 +1,32 @@
+import 'nodelist-foreach-polyfill';
+
+import calc from './modules/calc.js';
+import cards from './modules/cards.js';
+import forms from './modules/forms.js';
+import modal from './modules/modal.js';
+import slider from './modules/slider.js';
+import tabs from './modules/tabs.js';
+import timer from './modules/timer.js';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);   // открытие модального окна через определенный промежуток времени
+
+    calc();
+    cards();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrov: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2024-01-01');
+});
