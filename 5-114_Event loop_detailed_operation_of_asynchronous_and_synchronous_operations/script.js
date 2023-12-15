12a2cb9d1a77fe39936123f71a21e0cb9a973c7d
Taiga <taiga507@gmail.com>
2023-12-15 21:32:35 +0400

    5-115
    Macro and micro_tasks

--- 5-114_Event loop_detailed_operation_of_asynchronous_and_synchronous_operations/script.js
+++ 5-114_Event loop_detailed_operation_of_asynchronous_and_synchronous_operations/script.js
@@ -1,13 +1,19 @@
 'use strict';
 
-console.log(1);
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout 2');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('timeout 4');
+// }, 4000);
+
+// console.log(2);
 
 setTimeout(() => {
-    console.log('timeout 2');
-}, 2000);
-
-setTimeout(() => {
-    console.log('timeout 2');
-}, 2000);
+    console.log(1);
+}, 0);
 
 console.log(2);
